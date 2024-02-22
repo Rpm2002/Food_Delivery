bf890ba6d2db110f6afd6518bbfa41b5c10f0b03
Rhythm <rhythm758002@gmail.com>
2024-02-22 15:43:58 +0530

    changes made

---
+++ MyFoodie/src/Components/FoodCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { IoIosStar } from "react-icons/io"
+
+function FoodCard({id,img,name,price,desc,category,rating}) {
+  return (
+    <div className='font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg 
+    hover:scale-95 cursor-grab transition-all duration-150 ease-in-out gap-2'>
+        <img 
+          src={img}
+          alt=""
+          className='w-auto h-[130px]'
+        />
+        <div className='text-sm flex justify-between'>
+          <h2>{name}</h2>
+          <span className='text-violet-500 '>₹{price}</span>
+        </div>
+        <p className='text-sm font-normal'>{desc.slice(0,50)}...</p>
+        <div className='flex justify-between'>
+          <span className='flex justify-center items-center'>
+          <IoIosStar className='mr-1 text-yellow-400'/>{rating}
+          </span>
+          <button className='p-2 text-white bg-violet-600 hover:bg-violet-400 rounded-lg text-sm'>
+            Add to Cart
+          </button>
+        </div>
+
+    </div>
+  )
+}
+
+export default FoodCard
--- MyFoodie/src/Pages/Home.jsx
+++ MyFoodie/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import CategoryMenu from '../Components/CategoryMenu'
+import FoodItems from '../Components/FoodItems'
 
 function Home() {
   return (
     <>
       <Navbar/>
       <CategoryMenu/>
+      <FoodItems/>
     </>
   )
 }
